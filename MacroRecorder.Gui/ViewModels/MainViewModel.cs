9ee6ae664319632d7b032dccdd54b07dfe5f8d8e
mizuyoukan-ao <48982871+mizuyoukanao@users.noreply.github.com>
2026-01-03 17:10:31 +0900

    Add using directive for System.IO

--- MacroRecorder.Gui/ViewModels/MainViewModel.cs
+++ MacroRecorder.Gui/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.IO;
 using Microsoft.Win32;
 using MacroRecorder.Configuration;
 using MacroRecorder.Models;
